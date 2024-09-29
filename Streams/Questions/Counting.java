24bfe4059ae3c86047e47bfd9aa4fe7a4f157a2c
SahilM-Bsw <98315268+SahilM-Bsw@users.noreply.github.com>
2024-09-29 23:50:54 +0530

    Questons on streams

--- Streams/Questions/Counting.java
+++ Streams/Questions/Counting.java
@@ -1,13 +1,21 @@
 package Streams.Questions;
 
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Counting {
     public static void main(String[] args) {
         String str = "Hello World";
 
-        Map<Character, Long> frequency = str
+        List<Integer> list= Arrays.asList(1,1,1,1,1,3,4,5,2,6,4,8,5,3,7,3);
+
+        Map<Integer, Long> frequency = list.stream()
+        .collect(Collectors.groupingBy(n -> n, Collectors.counting()));
+
+
+
+
+        Map<Character, Long> frequencyChar = str
                                             .chars()
                                             .mapToObj(c -> (char) c)
                                             .filter(c -> c != ' ')
@@ -15,5 +23,7 @@ public class Counting {
                                             .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
 
         frequency.forEach((ch,fr) -> System.out.println(ch + ": " + fr));
+        frequencyChar.forEach((ch,fr) -> System.out.println(ch + ": " + fr));
+
     }
 }
